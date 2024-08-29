2f72d742a973cb8987b56b91bba5dee1362c5329
dongwei1125 <2465204327@qq.com>
2024-08-29 22:42:16 +0800

    add: 初步新增 exporess 接口数据

--- mock/server.js
+++ mock/server.js
@@ -1,3 +1,9 @@
-module.exports = function (middlewares, devServer) {
+module.exports = (middlewares, devServer) => {
+  devServer.app.get('/api/user/login', (request, response) => {
+    response.json({
+      xxx: 'xxx',
+    })
+  })
+
   return middlewares
 }
