bcfeae48cca5ba700728251ed1edfa44283fe1da
dongwei1125 <2465204327@qq.com>
2025-08-28 23:41:57 +0800

    fix: chokidar 路由监听异常

--- mock/app.js
+++ mock/app.js
@@ -1,23 +1,37 @@
 const chokidar = require('chokidar')
 const { join } = require('path')
-const { registerRoutes } = require('./routes')
+
 const responseWrapper = require('./responseWrapper')
 
-const dir = join(process.cwd(), 'mock')
+const dir = join(process.cwd(), 'mock/routes')
 
-function unregisterRoutes(app) { }
+function registerRoutes(app) {
+  const user = require('./routes/user')
+  const article = require('./routes/article')
+
+  app.use('/api/user', user)
+  app.use('/api/article', article)
+}
+
+function unregisterRoutes(app) {
+  // 清除模块缓存
+  Object.keys(require.cache).forEach(i => {
+    if (i.includes(dir)) {
+      delete require.cache[require.resolve(i)]
+    }
+  })
+
+  // 清除路由栈
+  app._router.stack = app._router.stack.filter(layer => layer.name !== 'router')
+}
 
 module.exports = app => {
   app.use(responseWrapper)
 
   registerRoutes(app)
 
-  chokidar
-    .watch(dir, {
-      ignored: /server/,
-    })
-    .on('all', (event, path) => {
-      unregisterRoutes(app)
-      registerRoutes(app)
-    })
+  chokidar.watch(dir).on('all', () => {
+    unregisterRoutes(app)
+    registerRoutes(app)
+  })
 }
